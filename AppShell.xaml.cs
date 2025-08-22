f5e175c65fb2d16add39da8809287d614c6569ce
jeriko.ichtus <jeriko.ichtus@gaws-it.com>
2025-08-22 14:27:12 +0700

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp_Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TodoApp_Maui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TodoApp_Maui.Pages")]
